0cb7d06337c0ecf8c895a9d76927f7bcc52e8d95
Fabian Ferreira <fab14an.fp@gmail.com>
2025-11-01 12:08:04 -0400

    Complete exercise 06_repeatString Pass all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let strRepeated = ""
+    for(let i = 0; i < num; i++) {
+        strRepeated += str
+    }
+    return strRepeated
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
