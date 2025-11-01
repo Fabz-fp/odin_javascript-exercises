53a0399ea340e38f6aa5aea4a9f841bbe43c0bfc
Fabian Ferreira <fab14an.fp@gmail.com>
2025-11-01 12:30:13 -0400

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
 
+    for(let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 };
 
+reverseString("hello there")
 // Do not edit below this line
 module.exports = reverseString;
