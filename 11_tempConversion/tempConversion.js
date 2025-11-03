b998276f88b954cb1fc33808e1f943895b100778
Fabian Ferreira <fab14an.fp@gmail.com>
2025-11-03 12:14:58 -0400

    Complete exercise 11_tempConversion Pass all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const toCelsius = Math.round(((temp - 32) * 5 / 9) * 10) / 10;
+  return toCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const toFahrenheit = Math.round(((temp * 9 / 5) + 32) * 10) / 10;
+  return toFahrenheit;
 };
 
 // Do not edit below this line
