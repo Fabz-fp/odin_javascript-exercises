bc00ef1c78892f2f6099831773370ca554a1faba
Fabian Ferreira <fab14an.fp@gmail.com>
2025-11-26 16:26:09 -0400

    Complete exercise 13_palindromes Pass all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let ogStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
+    let reversedStr = ogStr.split("").reverse().join("");
+
+    return ogStr === reversedStr;
 };
 
 // Do not edit below this line
