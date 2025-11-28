42882288a3fe6a669fb36d0147154680aaf38df9
Fabian Ferreira <fab14an.fp@gmail.com>
2025-11-28 16:09:06 -0400

    Complete exercise 14_fibonacci Pass all tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1,1];
 
+    if(typeof num === "string") {
+        num = parseInt(num);
+    }
+
+    if(num < 0) return "OOPS";
+    else if(num === 0) return 0;
+    else if(num === 1 || num === 2) return 1;
+    else {
+        for(let i = 2; i < num; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+    }
+    
+    return fib[num-1];
 };
 
 // Do not edit below this line
